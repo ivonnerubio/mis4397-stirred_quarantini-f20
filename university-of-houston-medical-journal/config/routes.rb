30a2f92965f814e359cfc04121295ed37ded1c56
ivonnerubio <irubio3@uh.edu>
2020-11-05 20:00:32 +0000

    adding users

--- university-of-houston-medical-journal/config/routes.rb
+++ university-of-houston-medical-journal/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :sign_ins
   resources :join_nows
   resources :research_submissions
@@ -8,5 +9,7 @@ Rails.application.routes.draw do
   resources :specialties
   resources :articles
   resources :homes
+  
+  root to: "homes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
