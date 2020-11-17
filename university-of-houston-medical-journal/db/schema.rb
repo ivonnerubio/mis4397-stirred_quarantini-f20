51b276a19bfadaec7f5d8a95b45bf5a317a274ad
Farwa Khan <fbkhan2@uh.edu>
2020-11-17 19:05:21 +0000

    Edits to the blog page - added comments section

--- university-of-houston-medical-journal/db/schema.rb
+++ university-of-houston-medical-journal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201105194834) do
+ActiveRecord::Schema.define(version: 20201117182145) do
 
   create_table "abouts", force: :cascade do |t|
     t.string   "title"
@@ -71,6 +71,14 @@ ActiveRecord::Schema.define(version: 20201105194834) do
     t.string   "ad_space"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "blog_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
   end
 
   create_table "homes", force: :cascade do |t|
