1f00faf4ecf790137f106b8e8f1b6f2e8d6ce1df
Youenn J <yjosselin-berhau@uh.edu>
2020-11-22 23:10:53 +0000

    profile section updates

--- university-of-houston-medical-journal/db/schema.rb
+++ university-of-houston-medical-journal/db/schema.rb
@@ -131,16 +131,6 @@ ActiveRecord::Schema.define(version: 20201120033833) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "fake_profiles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "education"
-    t.text     "background"
-    t.text     "award"
-    t.text     "certification"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
   create_table "homes", force: :cascade do |t|
     t.string   "title"
     t.string   "categories"
