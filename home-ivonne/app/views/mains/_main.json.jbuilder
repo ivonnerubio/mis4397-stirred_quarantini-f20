f39ef75f3caadf9fcf47e99fe6d671626006f11f
Zahra Memon <Zkm0789@gmail.com>
2020-11-24 15:17:47 +0000

    removed home-ivonne , unecessary app.

--- home-ivonne/app/views/mains/_main.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! main, :id, :text, :created_at, :updated_at
-json.url main_url(main, format: :json)
--- home-ivonne/db/migrate/20201119015123_create_mains.rb
+++
@@ -1,9 +0,0 @@
-class CreateMains < ActiveRecord::Migration[5.0]
-  def change
-    create_table :mains do |t|
-      t.string :text
-
-      t.timestamps
-    end
-  end
-end
